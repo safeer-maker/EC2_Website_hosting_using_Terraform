a9c68d066eda3cfd6b0f95e65191af2f2e568d8f
Safeer Linux <bsee15055@itu.edu.pk>
2024-03-03 05:40:01 +0500

    EC2 sub net issue

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,20 +1,21 @@
 
-data "aws_default_vpc" "default" {}
+# data "aws_default_vpc" "default" {}
 
-data "aws_default_subnet" "default" {
-  vpc_id = data.aws_default_vpc.default.id
-}
+# data "aws_default_subnet" "default" {
+#   vpc_id = data.aws_default_vpc.default.id
+# }
 
-data "aws_default_security_group" "default" {
-  vpc_id = data.aws_default_vpc.default.id
-}
+# data "aws_default_security_group" "default" {
+#   vpc_id = data.aws_default_vpc.default.id
+# }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c94855ba95c71c99"  # Replace with your desired AMI ID
+  ami           = "ami-0440d3b780d96b29d"  # Replace with your desired AMI ID
   instance_type = "t2.micro"                # Replace with your desired instance type
   //key_name      = "my-key-pair"             # Replace with your desired key pair name
 
-  subnet_id     = var.subnet_1_id
+  //subnet_id     = var.subnet_1_id
+  //subnet_id     = module.vpc.public_subnet_1_id
   //vpc_security_group_ids = var.security_group_http
 }
 
---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+security_group_http = "sg-0087d68fb33754bfd"
+subnet_1_id = "sg-0087d68fb33754bfd"
