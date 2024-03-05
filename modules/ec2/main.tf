75a2276e0bd9ee6b49641eeeda53c6b7c3f4585a
Safeer Linux <bsee15055@itu.edu.pk>
2024-03-05 19:17:22 +0500

    doc updated

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -3,15 +3,14 @@ locals{
 }
 
 
-resource "aws_instance" "example" {
-  ami           = "ami-0440d3b780d96b29d"  # Replace with your desired AMI ID
-  instance_type = "t2.micro"                # Replace with your desired instance type
-  key_name      = "my-key-pair"             # Replace with your desired key pair name
+resource "aws_instance" "ec2_t2.micro" {
+  ami           = "ami-0440d3b780d96b29d"
+  instance_type = "t2.micro"
+  key_name      = local.key_name
 
   subnet_id     =  var.subnet_1_id
 
   vpc_security_group_ids = [var.security_group_h]
 
 }
-
 
